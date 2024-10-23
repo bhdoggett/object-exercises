970385e82c2814e13d01f83d0479b911f38b2a39
Ben Doggett <bdoggett@gmail.com>
2024-10-22 22:53:12 -0400

    Add code to exercise 7 to satisfy tests

--- exercises/007.js
+++ exercises/007.js
@@ -33,24 +33,35 @@ Starter Code :
 */
 
 var customerData = {
-  'Joe': {
-    visits: 1
+  Joe: {
+    visits: 1,
   },
-  'Carol': {
-    visits: 2
+  Carol: {
+    visits: 2,
   },
-  'Howard': {
-    visits: 3
+  Howard: {
+    visits: 3,
   },
-  'Carrie': {
-    visits: 4
-  }
+  Carrie: {
+    visits: 4,
+  },
 };
 
 function greetCustomer(firstName) {
-  var greeting = '';
-  // your code here
+  var greeting = "";
 
+  if (customerData[firstName]) {
+    // console.log(customerData[firstName]);
+    if (customerData[firstName].visits === 1) {
+      greeting = `Welcome back, ${firstName}! We're glad you liked us the first time!`;
+    } else if (customerData[firstName].visits >= 2) {
+      greeting = `Welcome back, ${firstName}! So glad to see you again!`;
+    }
+  } else {
+    greeting = "Welcome! Is this your first time?";
+  }
   return greeting;
 }
 
+// var output = greetCustomer("Ben");
+// console.log(output);
